c6974e9f9c56623a1b772a110533514cae902924
ericahae <ericahasimpitiavana@gmail.com>
2023-02-10 08:28:49 +0300

    Exemple si ceci marche

--- gerer_chambre.php
+++ gerer_chambre.php
@@ -69,7 +69,7 @@ unset($_POST);
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+                    <h5 class="modal-title" id="exampleModalLabel">Voulez vous vraiment quittrer?</h5>
                     <button class="close" type="button" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">×</span>
                     </button>
